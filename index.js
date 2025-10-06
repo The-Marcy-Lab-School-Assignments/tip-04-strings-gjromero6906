cd69e46deadb9a140b9e0dbe4ce8a9fce05e4ec3
gjromero6906 <gjr6906@gmail.com>
2025-10-06 10:15:40 -0400

    used for loop and reverse counting

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ You should not use the reverse method and you should not use the reverse method 
 
 The function should return a NEW string as the output. 
 */
-function reverseString(str) {}
+function reverseString(str) {
+    let newString ="";
+    for(let i = str.length-1;i>-1;i--){
+        newString += str.charAt(i);
+    }
+    return newString;
+}
 
 // Test the function
 console.log(reverseString("Hello")); // Output: "olleH"
